dc03031df0ac4db43668c77828afbf1da037d3d2
Brasero <brandonricci4@gmail.com>
2024-03-25 12:45:21 +0100

    comments

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css';
-import {calcNum} from './utils/utils.js'
 
 
+// Le props children permet de définir que le component s'attend à recevoir des "enfant"
+// Il pourra être utilisé pour rendre les components qui seront passée en tant qu'enfant
 function ButtonGroup({children}) {
 
     return (
@@ -11,6 +12,8 @@ function ButtonGroup({children}) {
     )
 }
 
+// Un component peut prendre n'importe quel props en entrée, un props est en lécture seule dans le component
+//la modification d'un props est donc impossible à l'intérieur d'un component
 export function Button({background, text}) {
 
     const elem = []
@@ -40,14 +43,16 @@ function App() {
         }
     ]
 
-
+    //Un component React ne doit rendre qu'une seule balise JSX qui pourra avoir une infinité d'enfant
   return (
       <>
           <nav>
               <ButtonGroup>
                   {
+                      //Lors d'un map en JSX chaque component retourné lors du map devra avoir un props
+                      //"key" unique pour chaque component
                       elem.map((el, index) => {
-                          return <Button key={index} {...el} />
+                          return <Button key={index} {...el} />;
                       } )
                   }
               </ButtonGroup>
