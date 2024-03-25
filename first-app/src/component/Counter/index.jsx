152d27c60f0906e13529ee66aa6eac267960b91c
Brasero <brandonricci4@gmail.com>
2024-03-25 16:54:23 +0100

    correction counter

--- first-app/src/component/Counter/index.jsx
+++ first-app/src/component/Counter/index.jsx
@@ -1,16 +1,36 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 const Counter = () => {
 
     const [counter, setCounter] = useState(0)
+    const [disabled, setDisabled] = useState(true)
 
-    const handleClick = () => {
+
+    const handlePlus = () => {
         setCounter(counter + 1)
     }
 
+    const handleMinus = () => {
+        setCounter(counter - 1)
+    }
+
+    useEffect(() => {
+        if(counter <= 0) {
+            setDisabled(true)
+            return
+        }
+        setDisabled(false)
+    }, [counter]);
+
+
+
     return (
         <div>
+            {
+                disabled && <span style={{color: "red"}}>Impossible de décrementé</span>
+            }
+            <button disabled={disabled} onClick={handleMinus}>-1</button>
             <div>{counter}</div>
-            <button onClick={handleClick}>+1</button>
+            <button onClick={handlePlus}>+1</button>
         </div>
     );
 };
